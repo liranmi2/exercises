678d508dcdeeca6a92ea079cad3144099e6de896
[liranmi2] <liran.michaelov@gmail.com>
2021-04-29 14:51:59 +0300

    ex4

---
+++ ex4/second/shirts.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+    <head>    
+        <title>Shirts</title>
+    </head>
+    <body>
+        <section>
+            You chose shirt in <?php echo $_GET["size"];?> size with <?php echo $_GET["color"];?> color
+        </section>
+    </body>
+</html>
